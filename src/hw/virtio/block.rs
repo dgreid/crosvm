76dbe693bc98edf2dab2f568c1a0be98887e4462
Stephen Barber <smbarber@chromium.org>
2017-06-21 15:04:16 -0700

    crosvm: refactor mmio device setup
    In preparation for adding a net device, factor out some of the common code
    for setting up an mmio device and jailing it.
    
    BUG=none
    TEST=cargo test
    
    Change-Id: I94f02e56a2b0938d860322b731d8b17a25357128

--- src/hw/virtio/block.rs
+++ src/hw/virtio/block.rs
@@ -5,6 +5,7 @@
 use std::convert::From;
 use std::fs::File;
 use std::io::{Result, Error, Seek, SeekFrom, Read, Write};
+use std::os::unix::io::{AsRawFd, RawFd};
 use std::result;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicUsize, Ordering};
@@ -236,6 +237,16 @@ impl Block {
 }
 
 impl VirtioDevice for Block {
+    fn keep_fds(&self) -> Vec<RawFd> {
+        let mut keep_fds = Vec::new();
+
+        if let Some(ref disk_image) = self.disk_image {
+            keep_fds.push(disk_image.as_raw_fd());
+        }
+
+        keep_fds
+    }
+
     fn device_type(&self) -> u32 {
         TYPE_BLOCK
     }
