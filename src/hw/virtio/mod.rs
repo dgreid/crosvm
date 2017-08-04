45952e0fd21fa4bedb7f2163424270ae66924541
Dylan Reid <dgreid@chromium.org>
2017-08-04 00:00:14 -0700

    crosvm: Add virtio random
    Provide the guest OS with /dev/random.
    
    Change-Id: I1323836392f3f1d59a6be276ce495e0d78ea9669
    Signed-off-by: Dylan Reid <dgreid@chromium.org>

--- src/hw/virtio/mod.rs
+++ src/hw/virtio/mod.rs
@@ -8,11 +8,13 @@ mod queue;
 mod mmio;
 mod block;
 mod net;
+mod rng;
 
 pub use self::queue::*;
 pub use self::mmio::*;
 pub use self::block::*;
 pub use self::net::*;
+pub use self::rng::*;
 
 const DEVICE_ACKNOWLEDGE: u32 = 0x01;
 const DEVICE_DRIVER: u32 = 0x02;
@@ -20,8 +22,10 @@ const DEVICE_DRIVER_OK: u32 = 0x04;
 const DEVICE_FEATURES_OK: u32 = 0x08;
 const DEVICE_FAILED: u32 = 0x80;
 
+// Types taken from linux/virtio_ids.h
 const TYPE_NET: u32 = 1;
 const TYPE_BLOCK: u32 = 2;
+const TYPE_RNG: u32 = 4;
 
 const INTERRUPT_STATUS_USED_RING: u32 = 0x1;
 
