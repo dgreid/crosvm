d096fbb56ae498a244189538f4b370cd27944446
Dylan Reid <dgreid@chromium.org>
2018-08-19 06:21:38 +0000

    wip

--- qcow/src/qcow.rs
+++ qcow/src/qcow.rs
@@ -634,14 +634,8 @@ impl QcowFile {
         if let Some(free_cluster) = avail_clusters.pop() {
             return Ok(free_cluster);
         }
-        // Determine where the new end of the file should be and set_len, which
-        // translates to truncate(2).
-        let file_end: u64 = file.seek(SeekFrom::End(0))?;
-        let cluster_size: u64 = cluster_size;
-        let new_cluster_address: u64 = (file_end + cluster_size - 1) & !cluster_mask;
-        file.set_len(new_cluster_address + cluster_size)?;
 
-        Ok(new_cluster_address)
+        self.raw_file.add_cluster_end()
     }
 
     // Allocate and initialize a new data cluster. Returns the offset of the
@@ -880,68 +874,6 @@ fn offset_is_cluster_boundary(offset: u64, cluster_bits: u32) -> Result<()> {
         return Err(Error::InvalidOffset(offset));
     }
     Ok(())
-}
-
-// Writes the L1 table to `file`. Non-zero table entreis have `non_zero_flags` ORed before writing.
-fn write_pointer_table(
-    file: &mut File,
-    offset: u64,
-    table: &Vec<u64>,
-    non_zero_flags: u64,
-) -> std::io::Result<()> {
-    file.seek(SeekFrom::Start(offset))?;
-    for addr in table {
-        let val = if *addr == 0 {
-            0
-        } else {
-            *addr | non_zero_flags
-        };
-        file.write_u64::<BigEndian>(val)?;
-    }
-    Ok(())
-}
-
-// Reads the L1 table from the file and returns a Vec containing the table.
-fn read_pointer_table(
-    f: &mut File,
-    offset: u64,
-    size: usize,
-    mask: Option<u64>,
-) -> std::io::Result<Vec<u64>> {
-    let mut table = Vec::with_capacity(size as usize);
-    f.seek(SeekFrom::Start(offset))?;
-    let mask = mask.unwrap_or(0xffff_ffff_ffff_ffff);
-    for _ in 0..size {
-        table.push(f.read_u64::<BigEndian>()? & mask);
-    }
-    Ok(table)
-}
-
-// Writes a refcount block to the file.
-fn write_refcount_block(
-    file: &mut File,
-    offset: u64,
-    table: &VecCache<u16>,
-) -> std::io::Result<()> {
-    file.seek(SeekFrom::Start(offset))?;
-    for count in table.addrs() {
-        file.write_u16::<BigEndian>(*count)?;
-    }
-    Ok(())
-}
-
-// Read a refcount block from the file and returns a Vec containing the table.
-fn read_refcount_block(
-    f: &mut File,
-    offset: u64,
-    size: usize,
-) -> std::io::Result<VecCache<u16>> {
-    let mut table = Vec::with_capacity(size as usize);
-    f.seek(SeekFrom::Start(offset))?;
-    for _ in 0..size {
-        table.push(f.read_u16::<BigEndian>()?);
-    }
-    Ok(VecCache::from_vec(table))
 }
 
 // Ceiling of the division of `dividend`/`divisor`.
