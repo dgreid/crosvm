12de0e6344a0022a35f423eaedbddda2e38236fa
Dylan Reid <dgreid@chromium.org>
2018-10-04 20:05:59 -0700

    cold reset blocks writes
    Change-Id: I018a88890815097a10780eb271744b93c0af04b6

--- devices/tests/ac97.rs
+++ devices/tests/ac97.rs
@@ -7,15 +7,24 @@ extern crate sys_util;
 
 use devices::Ac97;
 
+const GLOB_CNT: u64 = 0x2c;
+
 #[test]
 fn bm_bdbar() {
     let mut ac97 = Ac97::new();
 
     let bdbars = [0x00u64, 0x10, 0x20];
 
+    // Make sure writes have no affect during cold reset.
+    ac97.bm_writel(0x00, 0x5555_555f);
+    assert_eq!(ac97.bm_readl(0x00), 0x0000_0000);
+ 
+    // Relesase cold reset.
+    ac97.bm_writel(GLOB_CNT, 0x0000_0002);
+
     // Tests that the base address is writable and that the bottom three bits are read only.
     for bdbar in &bdbars {
-        assert_eq!(ac97.bm_readl(*bdbar), 0x00);
+        assert_eq!(ac97.bm_readl(*bdbar), 0x0000_0000);
         ac97.bm_writel(*bdbar, 0x5555_555f);
         assert_eq!(ac97.bm_readl(*bdbar), 0x5555_5558);
     }
@@ -38,25 +47,23 @@ fn bm_status_reg() {
 fn bm_global_control() {
     let mut ac97 = Ac97::new();
 
-    let glob_cnt = 0x2cu64;
-
-    assert_eq!(ac97.bm_readl(glob_cnt), 0x0000_0000);
+    assert_eq!(ac97.bm_readl(GLOB_CNT), 0x0000_0000);
 
     // Relesase cold reset.
-    ac97.bm_writel(glob_cnt, 0x0000_0002);
+    ac97.bm_writel(GLOB_CNT, 0x0000_0002);
 
     // Check interrupt enable bits are writable.
-    ac97.bm_writel(glob_cnt, 0x0000_0072);
-    assert_eq!(ac97.bm_readl(glob_cnt), 0x0000_0072);
+    ac97.bm_writel(GLOB_CNT, 0x0000_0072);
+    assert_eq!(ac97.bm_readl(GLOB_CNT), 0x0000_0072);
 
     // A Warm reset should doesn't affect register state and is auto cleared.
     ac97.bm_writel(0x00, 0x5555_5558);
-    ac97.bm_writel(glob_cnt, 0x0000_0076);
-    assert_eq!(ac97.bm_readl(glob_cnt), 0x0000_0072);
+    ac97.bm_writel(GLOB_CNT, 0x0000_0076);
+    assert_eq!(ac97.bm_readl(GLOB_CNT), 0x0000_0072);
     assert_eq!(ac97.bm_readl(0x00), 0x5555_5558);
     // Check that a cold reset works, but setting bdbar and checking it is zeroed.
     ac97.bm_writel(0x00, 0x5555_555f);
-    ac97.bm_writel(glob_cnt, 0x000_0070);
-    assert_eq!(ac97.bm_readl(glob_cnt), 0x0000_0070);
+    ac97.bm_writel(GLOB_CNT, 0x000_0070);
+    assert_eq!(ac97.bm_readl(GLOB_CNT), 0x0000_0070);
     assert_eq!(ac97.bm_readl(0x00), 0x0000_0000);
 }
