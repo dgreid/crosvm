a15d07fdea932eb8576302d6c42eaff6797431d4
Dylan Reid <dgreid@chromium.org>
2018-10-11 09:14:24 -0700

    start playback
    Change-Id: Id756811add7515064d00f780535535e241f688c1

--- devices/tests/ac97.rs
+++ devices/tests/ac97.rs
@@ -5,6 +5,8 @@
 extern crate devices;
 extern crate sys_util;
 
+use std::time;
+
 use devices::Ac97;
 
 const GLOB_CNT: u64 = 0x2c;
@@ -70,13 +72,31 @@ fn bm_global_control() {
 
 #[test]
 fn test_measure_clock() {
-    const LVI_MASK: u32 = 0x1f; // Five bits for 32 total entries.
+    const LVI_MASK: u8 = 0x1f; // Five bits for 32 total entries.
     const IOC_MASK: u32 = 0x8000_0000; // Interrupt on completion.
     let num_buffers = LVI_MASK as usize + 1;
     let mut bdbar: Vec<u32> = vec![0; num_buffers * 2]; // Each entry is a pointer and a length.
     const buffer_size: usize = 1024 * 2;
+
+    // Initialize PO registers.
+    const PO_BASE: u64 = 0x10;
+    const PO_BDBAR: u64 = PO_BASE;
+    const PO_CIV: u64 = PO_BASE + 0x4;
+    const PO_LVI: u64 = PO_BASE + 0x5;
+    const PO_SR: u64 = PO_BASE + 0x6;
+    const PO_PICB: u64 = PO_BASE + 0x8;
+    const PO_PIV: u64 = PO_BASE + 0xa;
+    const PO_CR: u64 = PO_BASE + 0xb;
+    const CR_RPBM: u8 = 0x01;
+
+    let mut ac97 = Ac97::new();
+
+    // Release cold reset.
+    ac97.bm_writel(GLOB_CNT, 0x0000_0002);
+
     // Setup ping-pong buffers. A and B repeating for every possible index.
     let buffer: Vec<i16> = vec![0; buffer_size];
+    ac97.bm_writel(PO_BDBAR, &buffer[0] as * const i16 as u32);
     let (buffer_a, buffer_b) = buffer.split_at(buffer_size/2);
     for i in 0..num_buffers {
         bdbar[i*2] = if i % 2 == 0 {
@@ -86,4 +106,20 @@ fn test_measure_clock() {
         };
         bdbar[i*2 + 1] = IOC_MASK | (buffer_size as u32 / 2);
     }
+
+    ac97.bm_writeb(PO_CIV, 0);
+    ac97.bm_writeb(PO_LVI, LVI_MASK);
+
+    // TODO(dgreid) - clear interrupts.
+    
+    // Start.
+    ac97.bm_writeb(PO_CR, CR_RPBM);
+
+    std::thread::sleep(time::Duration::from_millis(10));
+
+    assert_ne!(0, ac97.bm_readw(PO_PICB));
+    assert_ne!(0, ac97.bm_readw(PO_CIV));
+ 
+    // Stop.
+    ac97.bm_writeb(PO_CR, 0);
 }
