f65c42d14537763541fa5b63261c95d26b747b32
Dylan Reid <dgreid@chromium.org>
2019-04-11 22:50:57 -0700

    start gdb stub replies

--- gdbstub/src/gdbstub.rs
+++ gdbstub/src/gdbstub.rs
@@ -1,9 +1,10 @@
-use std::iter::IntoIterator;
+use std::iter::{self, IntoIterator};
 
 enum IterState {
     Start,
     Data,
-    Checksum,
+    Checksum1,
+    Checksum2,
     Done,
 }
 
@@ -24,6 +25,7 @@ where
 
     fn next(&mut self) -> Option<u8> {
         use IterState::*;
+
         match self.state {
             Start => {
                 self.state = Data;
@@ -35,13 +37,17 @@ where
                     Some(x)
                 }
                 None => {
-                    self.state = Checksum;
+                    self.state = Checksum1;
                     Some(b'#')
                 }
             },
-            Checksum => {
+            Checksum1 => {
+                self.state = Checksum2;
+                Some(hex_msb(self.checksum))
+            }
+            Checksum2 => {
                 self.state = Done;
-                Some(self.checksum)
+                Some(hex_lsb(self.checksum))
             }
             Done => None,
         }
@@ -61,8 +67,8 @@ where
     }
 }
 
-pub fn empty() -> GdbReply<Vec<u8>> {
-    GdbReply::new(Vec::new())
+pub fn empty() -> GdbReply<iter::Empty<u8>> {
+    GdbReply::new(iter::empty())
 }
 
 /*
@@ -71,16 +77,51 @@ pub fn empty() -> GdbReply<Vec<u8>> {
         Ok(())
     }
 }
-
-pub fn error(errno: u8) -> GdbReply {
-    GdbMessage { data: "${}#" }
-}
 */
 
+pub struct GdbErrorData {
+    errno: u8,
+    idx: usize,
+}
+
+impl Iterator for GdbErrorData {
+    type Item = u8;
+    fn next(&mut self) -> Option<u8> {
+        let ret = match self.idx {
+            0 => Some(b'e'),
+            1 => Some(hex_msb(self.errno)),
+            2 => Some(hex_lsb(self.errno)),
+            _ => None,
+        };
+        self.idx = self.idx.saturating_add(1);
+        ret
+    }
+}
+
+pub fn error(errno: u8) -> GdbReply<GdbErrorData> {
+    GdbReply::new(GdbErrorData { errno, idx: 0 })
+}
+
+fn ascii_byte(digit: u8) -> u8 {
+    match digit {
+        d if d <= 9 => d + b'0',
+        d if d <= 0xf => d + b'a',
+        _ => b'0',
+    }
+}
+fn hex_lsb(digit: u8) -> u8 {
+    ascii_byte(digit & 0x0f)
+}
+fn hex_msb(digit: u8) -> u8 {
+    ascii_byte(digit & 0xf0 >> 4)
+}
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(empty().collect::<Vec<u8>>(), b"$#00");
+        assert_eq!(error(0x55).collect::<Vec<u8>>(), b"$e55#00");
     }
 }
