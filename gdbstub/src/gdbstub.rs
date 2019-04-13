3fb3e49c97a2978c12b9750e2ebaf49f51fea14a
Dylan Reid <dgreid@chromium.org>
2019-04-12 22:23:23 -0700

    hack: start message iterator
    Change-Id: Ie3a9a9d600369e994b06b20cb43feb002a31e39d

--- gdbstub/src/gdbstub.rs
+++ gdbstub/src/gdbstub.rs
@@ -4,6 +4,47 @@ fn handle_msg(msg: &[u8]) -> impl Iterator<Item = u8> {
     gdbreply::empty()
 }
 
+enum MessageState {
+    Idle,
+    RecievePacket(Vec<u8>),
+    ReceiveChecksum(Vec<u8>, Option<u8>, Option<u8>),
+}
+
+struct GdbMessage {
+    packet_data: &[u8],
+}
+
+struct MessageReader<T>
+where
+    T: Read,
+{
+    source: T,
+}
+
+impl<T> Iterator for MessageReader<T> {
+    type Item = GdbMessage;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        use MessageState::*;
+
+        loop {
+            let mut bytes = [0u8; 1];
+            self.source.read(&mut bytes);
+
+            let byte = bytes[0];
+
+            match self.state {
+                Idle => {
+                    if byte == b'$' {
+                        self.state = ReceiveChecksum(Vec::new());
+                    }
+                }
+                ReceiveChecksum(v) => {
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
