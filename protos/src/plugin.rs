7a6a7469d453cc75e097a969514774135ecdb9c2
Dylan Reid <dgreid@chromium.org>
2019-05-02 13:46:59 -0600

    protos: Use kvm-bindings crate
    The rust-vmm projects has moved to kvm-bindings instead of using
    kvm_sys. Move to the upstream crate.
    
    TEST=still builds
    CqDepend=chromium:1593924
    
    Signed-off-by: Dylan Reid <dgreid@chromium.org>

--- protos/src/plugin.rs
+++ protos/src/plugin.rs
@@ -5,12 +5,12 @@
 pub use crate::generated::plugin::*;
 
 /// Converts protobuf representation of CpuId data into KVM format.
-pub fn cpuid_proto_to_kvm(entry: &CpuidEntry) -> kvm_sys::kvm_cpuid_entry2 {
+pub fn cpuid_proto_to_kvm(entry: &CpuidEntry) -> kvm_bindings::kvm_cpuid_entry2 {
     // Safe: C structures are expected to be zero-initialized.
-    let mut e: kvm_sys::kvm_cpuid_entry2 = unsafe { std::mem::zeroed() };
+    let mut e: kvm_bindings::kvm_cpuid_entry2 = unsafe { std::mem::zeroed() };
     e.function = entry.function;
     if entry.has_index {
-        e.flags = kvm_sys::KVM_CPUID_FLAG_SIGNIFCANT_INDEX;
+        e.flags = kvm_bindings::KVM_CPUID_FLAG_SIGNIFCANT_INDEX;
     }
     e.index = entry.index;
     e.eax = entry.eax;
@@ -22,10 +22,10 @@ pub fn cpuid_proto_to_kvm(entry: &CpuidEntry) -> kvm_sys::kvm_cpuid_entry2 {
 }
 
 /// Converts KVM representation of CpuId data into protobuf format.
-pub fn cpuid_kvm_to_proto(entry: &kvm_sys::kvm_cpuid_entry2) -> CpuidEntry {
+pub fn cpuid_kvm_to_proto(entry: &kvm_bindings::kvm_cpuid_entry2) -> CpuidEntry {
     let mut e = CpuidEntry::new();
     e.function = entry.function;
-    e.has_index = entry.flags & kvm_sys::KVM_CPUID_FLAG_SIGNIFCANT_INDEX != 0;
+    e.has_index = entry.flags & kvm_bindings::KVM_CPUID_FLAG_SIGNIFCANT_INDEX != 0;
     e.index = entry.index;
     e.eax = entry.eax;
     e.ebx = entry.ebx;
