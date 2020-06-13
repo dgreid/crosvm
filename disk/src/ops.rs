95fa4b5194e1b49029cd57d835f44653f2d36206
Dylan Reid <dgreid@chromium.org>
2020-06-13 12:44:59 -0700

    wip
    Change-Id: If9d5edcdb8d24bfe2005e4ae723dfc91a06d1618

--- disk/src/ops.rs
+++ disk/src/ops.rs
@@ -1,7 +1,10 @@
 //! Operations that can be preformed on disks.
 
+use std::marker::PhantomData;
+use std::os::unix::io::RawFd;
+
 /// The operation types and the data needed to start each.
-pub enum OperationType {
+pub enum DiskOperation {
     FSync,
     PunchHole {
         offset: u64,
@@ -22,11 +25,11 @@ pub enum OperationType {
     },
 }
 
-impl OperationType {
+impl DiskOperation {
     /// Returns true if this operation is allowed on read-only disks.
     pub fn valid_read_only(&self) -> bool {
         match self {
-            OperationType::Read {
+            DiskOperation::Read {
                 offset: _offset,
                 len: _len,
             } => true, // Read works on read-only disks.
@@ -36,4 +39,21 @@ impl OperationType {
             }
         }
     }
+}
+
+/// Operations that can be performed on the backing FD of a disk. The backing FD could be a single
+/// FD for a File, multiple FDs for composite, or multiple FDs at multple locations for qcow.
+pub struct BackingOperation<'a> {
+    fd: RawFd,
+    op: DiskOperation,
+    _owner_lifetime: PhantomData<&'a ()>,
+}
+
+/// Trait for objects that can process `DiskOperation`s and return operations to be performed on the
+/// backing disk representation.
+pub trait OpProcess {
+    fn process_op(
+        &self,
+        op: DiskOperation,
+    ) -> std::result::Result<&[BackingOperation<'_>], Box<dyn std::error::Error>>; // TODO iter not slice?
 }
