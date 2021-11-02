3b76133ede42057e13c45d6ce0c2e2714c26b4ee
Dylan Reid <dgreid@rivosinc.com>
2021-11-02 15:39:06 -0700

    RIVOS: hypervisor: add riscv64 kvm support
    Add basic support for linux kvm running on a riscv64 cpu.
    This will allow for co-development of crosvm with the riscv64
    virtualization architecture and kernel support.
    
    Change-Id: I77c091d9b30758788b7c97bc0b3804879e27055d

--- hypervisor/src/lib.rs
+++ hypervisor/src/lib.rs
@@ -11,6 +11,8 @@ pub mod caps;
 pub mod haxm;
 #[cfg(unix)]
 pub mod kvm;
+#[cfg(target_arch = "riscv64")]
+pub mod riscv64;
 #[cfg(all(windows, feature = "whpx"))]
 pub mod whpx;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
@@ -32,6 +34,8 @@ use vm_memory::GuestMemory;
 #[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
 pub use crate::aarch64::*;
 pub use crate::caps::*;
+#[cfg(target_arch = "riscv64")]
+pub use crate::riscv64::*;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub use crate::x86_64::*;
 
@@ -465,6 +469,19 @@ pub enum VcpuExit {
     ApicInitSipiTrap,
     /// vcpu stoppted due to bus lock
     BusLock,
+    /// Riscv supervisor call.
+    Sbi {
+        extension_id: u64,
+        function_id: u64,
+        args: [u64; 6],
+    },
+    /// Emulate CSR access from guest.
+    RiscvCsr {
+        csr_num: u64,
+        new_value: u64,
+        write_mask: u64,
+        ret_value: u64,
+    },
 }
 
 /// A hypercall with parameters being made from the guest.
@@ -493,6 +510,9 @@ pub enum DeviceKind {
     /// ARM virtual general interrupt controller v3
     #[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
     ArmVgicV3,
+    /// RiscV AIA in-kernel emulation
+    #[cfg(any(target_arch = "riscv64"))]
+    RiscvAia,
 }
 
 /// The source chip of an `IrqSource`
@@ -503,6 +523,7 @@ pub enum IrqSourceChip {
     PicSecondary,
     Ioapic,
     Gic,
+    Aia,
 }
 
 /// A source of IRQs in an `IrqRoute`.
