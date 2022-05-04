fbd2c5ad1e9ba56a9d535304a892ebce372bc786
Dylan Reid <dgreid@rivosinc.com>
2022-05-04 22:33:08 +0000

    RIVOS: Add rivos run scripts
    Change-Id: I5cfe3e33ce4d8c2beda234f36424dd708cbaaef4

---
+++ start_host_riscv.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+~/src/qemu/build/qemu-system-riscv64 -kernel ~/src/linux/arch/riscv/boot/Image -nographic -machine virt,aia=aplic-imsic,aia-guests=4 -m 4G  -append "root=/dev/vda ro console=ttyS0 earlycon=sbi keep_bootcon bootmem_debug"                 -bios ~/src/opensbi/build/platform/generic/firmware/fw_jump.bin                 -drive file=/home/dylan/src/buildroot/output/images/rootfs.ext2,format=raw,id=hd0                 -device virtio-blk-device,drive=hd0                 -virtfs local,path=./,mount_tag=host,security_model=none  -netdev user,id=network0,hostfwd=tcp::1234-:1234  -device e1000,netdev=network0,mac=52:54:00:54:46:69
+
